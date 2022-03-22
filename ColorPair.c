746b7a42a3d1a586420a8c887fcf9589771e6bee
Ruthrapathi <90901003+Ruthrapathi@users.noreply.github.com>
2022-03-22 23:56:03 +0530

    Create ColorPair.c

---
+++ ColorPair.c
@@ -0,0 +1,25 @@
+#include "ColorPair.h"
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+	
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+	
+    PrintManual();
+
+    return 0;
+}
